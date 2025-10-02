dbed9f9a04cccba384d9083a90de784ca193df26
ThatOneGin <176781942+ThatOneGin@users.noreply.github.com>
2025-10-02 08:09:28 -0300

    Update GC count properly when allocating/freeing memory.

--- src/cmem.c
+++ src/cmem.c
@@ -24,6 +24,10 @@ static void *_realloc(void *ptr, cmem_t size) {
   return NULL;
 }
 
+/*
+** If an allocation fails, try calling GC to free
+** some memory and try again.
+*/
 static void *tryagain(cyth_State *C, void *ptr, cmem_t size) {
   global_State *G = C->G;
   if (G->count >= GCTHRESHOLD) {
@@ -43,6 +47,7 @@ void cythM_error(cyth_State *C, const char *type, cmem_t size) {
   }
 }
 
+/* helper function to reallocate vectors */
 void cythM_grow(cyth_State *C, void **ptr,
                 cmem_t *size, cmem_t scalar, const char *type) {
   cmem_t old_size = *size;
@@ -56,6 +61,8 @@ void cythM_grow(cyth_State *C, void **ptr,
     if (tmp == NULL)
       cythM_error(C, type, *size);
   }
+  C->G->count += *size - old_size;
+  C->G->total += *size - old_size;
   *ptr = tmp;
 }
 
@@ -63,16 +70,23 @@ void *cythM_malloc(cyth_State *C, cmem_t size) {
   if (size == 0) return NULL;
   void *ptr = _realloc(NULL, size);
   if (ptr == NULL)
+    ptr = tryagain(C, ptr, size);
+  if (ptr == NULL)
     cythM_error(C, NULL, size);
+  C->G->count += size;
+  C->G->total += size;
   return ptr;
 }
 
 void *cythM_realloc(cyth_State *C, void *ptr, cmem_t oldsize, cmem_t size) {
   if (size == 0)
     C->G->count -= oldsize;
-  else {
+  else if (size < oldsize) {
+    C->G->count -= (oldsize - size);
+  } else {
     cmem_t count = size - oldsize;
     C->G->count += count;
+    C->G->total += count;
   }
   return _realloc(ptr, size);
 }
